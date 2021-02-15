a1c73a71ab222e81226d01b3e153aaef40f94e36
Andres <andresmunozgo@gmail.com>
2021-02-15 12:19:00 -0300

    Create a user control panel that only admin users will have access to, this control panel. admin users will be able to assign or remove administrator privileges

---
+++ app/controllers/admin_users_controller.rb
@@ -0,0 +1,19 @@
+class AdminUsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def make_admin
+        @user = User.find(params[:format])
+        @user.update(admin: true)
+        redirect_to admin_users_index_path
+    end
+    
+    def revoke_admin
+        @user = User.find(params[:format])
+        @user.update(admin: false)
+        redirect_to admin_users_index_path
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
+  
+  get 'admin_users/index'
+  get 'admin_users/revoke_admin'
+  get 'admin_users/make_admin'
+  
   devise_for :users
   resources :stories
   root 'home#index'
